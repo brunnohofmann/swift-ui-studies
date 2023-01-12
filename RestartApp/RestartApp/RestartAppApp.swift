46f0bdaa54374e8419b876d74cca40151cd82ebc
brunnohofmann <brunnohofmann@hotmail.com>
2023-01-12 22:21:23 +0100

    Initial Structure

---
+++ RestartApp/RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Brunno Hofmann on 12.01.23.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
