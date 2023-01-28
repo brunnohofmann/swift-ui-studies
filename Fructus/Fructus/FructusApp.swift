57e09818b7ffd14957538a8634a1988a64774a77
brunnohofmann <brunnohofmann@hotmail.com>
2023-01-28 11:50:49 +0100

    Initial Structure for Fructus app

---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Brunno Hofmann on 28.01.23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
